f1a49a504492638889d7506f84bc9a829d2d6d66
Knarik999 <57656929+Knarik999@users.noreply.github.com>
2019-11-21 13:11:57 +0400

    changed utils and deleted matrix.js

--- utils.js
+++ utils.js
@@ -24,6 +24,25 @@ function multiplyMatrices(matrix1, matrix2){
   }
 }
 
+function printAry(matrix) {
+
+  let answer = "";
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      answer += matrix[i][j] + '\t';
+    }
+    answer += "\n";
+  }
+  return answer;
+}
+
 module.exports = {
-  multiplyMatrices
+  printAry
+}
+
+
+module.exports = {
+  multiplyMatrices,
+  printAry
 };
